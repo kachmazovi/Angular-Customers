e0d8c3a269052e6316c01811f185384d673854c2
Kazbegi <84750518+kachmazovi@users.noreply.github.com>
2025-01-23 21:41:24 +0400

    add lifecycle hooks

--- customers/src/app/app.component.ts
+++ customers/src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, RegisterComponent],
+  imports: [LoginComponent, RegisterComponent, ChildComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('loginComponent') public loginComp!: ElementRef;
+
   public loginPage = true;
+
+  public title = '';
 
   constructor() {
     console.log('app component initted');
@@ -21,5 +32,20 @@ export class AppComponent {
 
   public register() {
     this.loginPage = false;
+  }
+
+  public ngOnInit(): void {
+    console.log('login component:', this.loginComp);
+  }
+
+  public ngAfterViewInit(): void {
+    console.log(
+      'login component after view init:',
+      this.loginComp.nativeElement
+    );
+  }
+
+  public changeTitle() {
+    this.title = Math.random().toFixed(2).toString();
   }
 }
--- customers/src/app/login/login.component.ts
+++ customers/src/app/login/login.component.ts
@@ -1,26 +1,98 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  Component,
+  DoCheck,
+  EventEmitter,
+  input,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { LoginRegister } from '../login-register-form/login-register.class';
 import { FormsModule } from '@angular/forms';
+import { NgClass, NgStyle } from '@angular/common';
+import { HighlightDirective } from './highlight.directive';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass, NgStyle, HighlightDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent extends LoginRegister {
+export class LoginComponent
+  extends LoginRegister
+  implements
+    OnInit,
+    OnChanges,
+    OnDestroy,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked
+{
   @Output() register = new EventEmitter();
 
-  public title = 'Login';
+  @Input() title = 'Login';
 
   public username!: string;
   public password!: string;
+
+  public isSpecial = true;
+  public currentClasses = {
+    isSpecial: this.isSpecial,
+    user: this.username,
+    pass: this.password,
+  };
+
+  public currentStyles = {
+    'font-style': true ? 'italic' : 'normal',
+    'font-weight': false ? 'bold' : 'normal',
+    'font-size': this.isSpecial ? '24px' : '12px',
+  };
 
   constructor() {
     super();
   }
 
+  public ngOnInit(): void {
+    console.log('on init');
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
+  public ngOnDestroy(): void {
+    console.log('component destroyed');
+  }
+
+  public ngDoCheck(): void {
+    console.log('do check');
+  }
+
+  public ngAfterContentInit(): void {
+    console.log('after content init');
+  }
+
+  public ngAfterContentChecked(): void {
+    console.log('after content checked');
+  }
+
   public login() {
     console.log(this.username, this.password);
+    this.currentClasses = {
+      isSpecial: this.isSpecial,
+      user: this.username,
+      pass: this.password,
+    };
+
+    this.currentStyles = {
+      'font-style': 'normal',
+      'font-weight': 'bold',
+      'font-size': '32px',
+    };
   }
 }
